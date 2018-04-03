deefbe26ee8d708cb744b378932c7d955279e168
temiooo <temitope.joloko@gmail.com>
2018-04-03 19:24:08 +0100

    feat(fizzbuzz): write code for fizzbuzz method
    - implement minimum functionality for fizzbuzz to ensure tests pass

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,25 @@
-def fizzbuzz
+def fizzbuzz(number = nil)
+  if number.nil?
+    return "Error: No parameter specified"
+  elsif !number.is_a? Numeric
+    return "Error: Parameter is not a number"
+  else
+    checkfizzbuzz(number)
+  end
+end
+
+def checkfizzbuzz(number)
+  if(number%3 == 0 && number%5==0)
+    return "Fizzbuzz"
+  end
+
+  if(number%3 == 0)
+    return "Fizz"
+  end
+
+  if(number%5 == 0)
+    return "Buzz"
+  end
+
+  return number
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,12 +6,12 @@ describe "Fizzbuzz" do
   end
 
   it "throws an error if parameter is not a number" do
-    expect(fizzbuzz("middle")).to eq("Error: Parameter is not a number")
+    expect(fizzbuzz("")).to eq("Error: Parameter is not a number")
   end
 
   it "returns Fizz for multiples of 3" do
     expect(fizzbuzz(3)).to eq("Fizz")
-    expect(fizzbuzz(36)).to eq("FIzz")
+    expect(fizzbuzz(36)).to eq("Fizz")
   end
 
   it "returns Buzz for multiples of 5" do
@@ -19,13 +19,13 @@ describe "Fizzbuzz" do
     expect(fizzbuzz(100)).to eq("Buzz")
   end
 
-  it "returns FizzBuzz for multiples of 3 and 5" do
+  it "returns Fizzbuzz for multiples of 3 and 5" do
     expect(fizzbuzz(30)).to eq("Fizzbuzz")
     expect(fizzbuzz(75)).to eq("Fizzbuzz")
   end
 
   it "returns the input if input is not a multiple of 3, 5 or both" do
     expect(fizzbuzz(1)).to eq(1)
-    expect(fizzbuzz(42)).to eq(42)
+    expect(fizzbuzz(46)).to eq(46)
   end
 end
